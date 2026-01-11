3b3dfb1b17599313b76b50e398db4948b2ef9df0
Renmauzuo <renmauzuo@yahoo.com>
2026-01-11 13:41:31 -0600

    Add basic armor to arena fights
    It looked a little weird to have some characters fully nude during the arena melee fights. Combatants will now equip a plain arena tunic.

--- source/module_mission_templates.py
+++ source/module_mission_templates.py
@@ -7557,14 +7557,14 @@ mission_templates = [
       (30,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_heavy_practice_sword,itm_practice_horse,itm_arena_tunic_yellow]),
       (31,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_practice_lance,itm_practice_shield,itm_practice_horse,itm_arena_tunic_yellow, itm_gold_tourney_helmet]),
 #32
-      (32,mtef_visitor_source|mtef_team_1,af_override_all,aif_start_alarmed,1,[itm_heavy_practice_sword]),
-      (33,mtef_visitor_source|mtef_team_2,af_override_all,aif_start_alarmed,1,[itm_practice_staff]),
-      (34,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_practice_sword, itm_practice_shield]),
-      (35,mtef_visitor_source|mtef_team_4,af_override_all,aif_start_alarmed,1,[itm_practice_staff]),
-      (36,mtef_visitor_source|mtef_team_1,af_override_all,aif_start_alarmed,1,[itm_practice_bow,itm_practice_arrows_10_amount, itm_practice_dagger]), #SB : change this to lower ammo amount
-      (37,mtef_visitor_source|mtef_team_2,af_override_all,aif_start_alarmed,1,[itm_practice_sword, itm_practice_shield]),
-      (38,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_heavy_practice_sword]),
-      (39,mtef_visitor_source|mtef_team_4,af_override_all,aif_start_alarmed,1,[itm_practice_staff]),
+      (32,mtef_visitor_source|mtef_team_1,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_heavy_practice_sword]),
+      (33,mtef_visitor_source|mtef_team_2,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_practice_staff]),
+      (34,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_practice_sword, itm_practice_shield]),
+      (35,mtef_visitor_source|mtef_team_4,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_practice_staff]),
+      (36,mtef_visitor_source|mtef_team_1,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_practice_bow,itm_practice_arrows_10_amount, itm_practice_dagger]), #SB : change this to lower ammo amount
+      (37,mtef_visitor_source|mtef_team_2,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_practice_sword, itm_practice_shield]),
+      (38,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_heavy_practice_sword]),
+      (39,mtef_visitor_source|mtef_team_4,af_override_all,aif_start_alarmed,1,[itm_arena_tunic_white,itm_practice_staff]),
 #40-49 not used yet
       (24,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_practice_bow,itm_practice_arrows,itm_practice_horse,itm_arena_tunic_yellow, itm_gold_tourney_helmet]),
       (24,mtef_visitor_source|mtef_team_3,af_override_all,aif_start_alarmed,1,[itm_heavy_practice_sword,itm_arena_tunic_yellow, itm_gold_tourney_helmet]),
